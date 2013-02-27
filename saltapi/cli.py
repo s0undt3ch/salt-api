dde19f8814e25ffd39ed05e2334175eb494ff434
Pedro Algarvio <pedro@algarvio.me>
2013-02-27 12:36:45 +0000

    Make log file logging work.
    * Migrated `saltapi.cli.SaltAPI` to use the new `_config_filename_` attribute.
    * In order for the log file logging to work, we need to verify the environment like salt does, which will create the necessary directories with the proper permissions.
    * Remove unnecessary call to `process_config_dir()`.

--- saltapi/cli.py
+++ saltapi/cli.py
@@ -1,7 +1,13 @@
 '''
 CLI entry-point for salt-api
 '''
+# Import python libs
+import os
+import sys
+import getpass
+
 # Import salt libs
+from salt.utils.verify import verify_env, verify_files
 from salt.utils.parsers import (
     ConfigDirMixIn,
     DaemonMixIn,
@@ -9,7 +15,8 @@ from salt.utils.parsers import (
     MergeConfigMixIn,
     OptionParser,
     OptionParserMeta,
-    PidfileMixin)
+    PidfileMixin
+)
 
 # Import salt-api libs
 import saltapi.client
@@ -26,16 +33,48 @@ class SaltAPI(OptionParser, ConfigDirMixIn, LogLevelMixIn, PidfileMixin,
 
     VERSION = saltapi.version.__version__
 
+    # ConfigDirMixIn config filename attribute
+    _config_filename_ = 'master'
+
+    # LogLevelMixIn default log level attribute
+    _default_logging_level_ = 'info'
+
+    def _mixin_after_parsed(self):
+        # Remove log_level_logfile from config if set to None so it can be
+        # equal to console log_level
+        if self.config['log_level_logfile'] is None:
+            self.config.pop('log_level_logfile')
+
     def setup_config(self):
-        return saltapi.config.api_config(self.get_config_file_path('master'))
+        return saltapi.config.api_config(self.get_config_file_path())
 
     def run(self):
         '''
         Run the api
         '''
         self.parse_args()
-        self.process_config_dir()
+
+        try:
+            if self.config['verify_env']:
+                verify_env(
+                    [os.path.dirname(self.config['conf_file'])],
+                    getpass.getuser()
+                )
+                logfile = self.config['log_file']
+                if logfile is not None and (
+                        not logfile.startswith('tcp://') or
+                        not logfile.startswith('udp://') or
+                        not logfile.startswith('file://')):
+                    # Logfile is not using Syslog, verify
+                    verify_files([logfile], getpass.getuser())
+        except OSError as err:
+            sys.exit(err.errno)
+
+        # Setup log file logging
+        self.setup_logfile_logger()
+
         self.daemonize_if_required()
         self.set_pidfile()
+
         client = saltapi.client.SaltAPIClient(self.config)
         client.run()
